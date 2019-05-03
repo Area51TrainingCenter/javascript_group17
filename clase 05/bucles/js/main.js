1c956f2b4b21472c739ba6386f2bc0ed01c13646
Alumno <alumno@A51MM-04.local>
2019-05-02 19:26:37 -0500

    update

---
+++ clase 05/bucles/js/main.js
@@ -0,0 +1,38 @@
+
+listado=[1,2,3,4]; = 4
+
+for(let i=0;i<listado.length;i++){
+	listado[0]
+	listado[1]
+	listado[2]
+	listado[3]
+}
+
+for (var i = listado.length - 1; i >= 0; i--) {
+
+	listado[3]
+	listado[2]
+	listado[1]
+	listado[0]
+
+}
+
+
+listado=[{item:"Producto01",categoria:1},
+        {item:"Producto02",categoria:2},
+        {item:"Producto03",categoria:3},
+        {item:"Producto04",categoria:3},
+        {item:"Producto05",categoria:2},
+        {item:"Producto06",categoria:1},
+        {item:"Producto07",categoria:1},
+        {item:"Producto08",categoria:2},
+        {item:"Producto09",categoria:2},
+        {item:"Producto10",categoria:3},
+        {item:"Producto11",categoria:3},
+        {item:"Producto12",categoria:1},
+        {item:"Producto13",categoria:2}]
+
+categoria1=[]
+categoria2=[]
+categoria3=[]
+
